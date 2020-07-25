f600fcc8d34c0b51e83ec5bedf79483b12627f72
kaneko <kaneko@spica.sd.tmu.ac.jp>
2020-07-25 18:47:21 +0900

    Fix setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -29,9 +29,9 @@ echo Loading wi+locness
 wget -P ../data https://www.cl.cam.ac.uk/research/nl/bea2019st/data/wi+locness_v2.1.bea19.tar.gz
 tar -xzvf ../data/wi+locness_v2.1.bea19.tar.gz -C ../data
 
-python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABC.train.gold.bea19.m2 ../data/train.src .    ./data/train.trg
-python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABCN.dev.gold.bea19.m2 ../data/valid.src ..    /data/valid.trg
-python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABCN.dev.gold.bea19.m2 ../data/test.src ../    data/test.trg
+python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABC.train.gold.bea19.m2 ../data/train.src ../data/train.trg
+python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABCN.dev.gold.bea19.m2 ../data/valid.src ../data/valid.trg
+python -u convert_m2_to_parallel.py ../data/wi+locness/m2/ABCN.dev.gold.bea19.m2 ../data/test.src ../data/test.trg
 
 echo Loading pre-trained model
 mkdir -p ../pretrained
