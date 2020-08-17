62e0c25595044c1ed64fda6ee330f2748b35dfc7
kanekomasahiro <kaneko-masahiro@ed.tmu.ac.jp>
2020-08-17 19:23:35 +0900

    Fixed genearte.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,24 +1,28 @@
 input=$1
+gpu=$2
 beam=5
-bert_type=bert-base-cased
-seed=2222
-
-SUBWORD_NMT=../subword-nmt
+bert_type=../bert-fuse_ged/fine-tuned-bert
+SUBWORD_NMT=../subword
 FAIRSEQ_DIR=../bert-nmt
 BPE_MODEL_DIR=../gec-pseudodata/bpe
-MODEL_DIR=../model/$bert_type/$seed
-OUTPUT_DIR=$MODEL_DIR/output_$input
+MODEL_DIR=../bert-fuse_ged/gec_model
+OUTPUT_DIR=$MODEL_DIR/output
+PREPROCESS=../bert-fuse_ged/preprocess/bin
+
+mkdir -p $OUTPUT_DIR
 
 $SUBWORD_NMT/apply_bpe.py -c $BPE_MODEL_DIR/bpe_code.trg.dict_bpe8000 < $input > $OUTPUT_DIR/test.bpe.src
+
 python -u detok.py $input $OUTPUT_DIR/test.bert.src
 paste -d "\n" $OUTPUT_DIR/test.bpe.src $OUTPUT_DIR/test.bert.src > $OUTPUT_DIR/test.cat.src
 
 echo Generating...
-python -u ${FAIRSEQPY}/interactive.py ${PROCESSED_DIR}/bin \
+CUDA_VISIBLE_DEVICES=$gpu python -u ${FAIRSEQ_DIR}/interactive.py $PREPROCESS \
     --path ${MODEL_DIR}/checkpoint_best.pt \
     --beam ${beam} \
     --nbest ${beam} \
     --no-progress-bar \
+    -s src \
     --buffer-size 1024 \
     --batch-size 32 \
     --log-format simple \
